9a715b2e308b2059e19b7fd7a36ffcfedb474442
Victhorambrosio <victhor.ambrosio@aluno.senai.br>
2026-03-12 16:30:30 -0300

    Inicio e finalização de um projeto com react-router, contendo navbar, 4 páginas funcionais e rodapé com endereço ao github profile.

--- I3DS2026-A-main/I3DS-2026-A-main/06-react-router/src/App.jsx
+++ I3DS2026-A-main/I3DS-2026-A-main/06-react-router/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router";
+import "./App.css";
+
+import Sobre from "./pages/Sobre";
+import Home from "./pages/Home";
+import Header from "./components/header/Header";
+import Contato from "./pages/Contato";
+import NaoEncontrado from "./pages/NaoEncontrado";
+import Rodape from "./components/rodape/Rodape";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Header />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Routes>
+        {/* Identifica todas as rotas do sistema*/}
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} /> {/* Uma rota do sistema */}
+        <Route path="/Contato" element={<Contato />} />
+        <Route path="/NaoEncontrado" element={<NaoEncontrado />} />
+      </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Rodape link={"https://github.com/Victhorambrosio"}>Victhor Ambrósio</Rodape>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
