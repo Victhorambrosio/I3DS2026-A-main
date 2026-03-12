fa949ec6a2499151aa455a438efaa12fadebbf9c
Victhorambrosio <victhor.ambrosio@aluno.senai.br>
2026-03-12 17:01:06 -0300

    feat: initialize React project with routing and basic components
    - Add package.json with dependencies for React, React Router, and Vite.
    - Create main application structure with App component and routing setup.
    - Implement Header and Footer components for navigation and footer display.
    - Add pages: Home, Sobre, Contato, and NaoEncontrado for routing.
    - Style application with CSS and Bootstrap for a responsive layout.
    - Configure Vite for development server and build process.

---
+++ I3DS2026-A-main/I3DS-2026-A-main/06-react-router-pro-max/src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router";
+import "./App.css";
+
+import Sobre from "./pages/Sobre";
+import Home from "./pages/Home";
+import Header from "./components/header/Header";
+import Contato from "./pages/Contato";
+import NaoEncontrado from "./pages/NaoEncontrado";
+import Rodape from "./components/rodape/Rodape";
+
+const App= ()=> {
+  return (
+    <>
+      <Header />
+      <div>
+      <Routes>
+        {/* Identifica todas as rotas do sistema*/}
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} /> {/* Uma rota do sistema */}
+        <Route path="/Contato" element={<Contato />} />
+        <Route path="*" element={<NaoEncontrado />} />
+      </Routes>
+      </div>
+      <Rodape link={"https://github.com/Victhorambrosio"}>Victhor Ambrósio</Rodape>
+
+    </>
+  );
+}
+
+export default App;
